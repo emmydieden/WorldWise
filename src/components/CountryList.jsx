5449bf22e98a3d595b41e1199c5e908d680339aa
emmydieden <emmy.dieden@gmail.com>
2024-03-06 10:09:09 +0100

    initial commit

---
+++ src/components/CountryList.jsx
@@ -0,0 +1,22 @@
+import Spinner from "./Spinner";
+import CityItem from "./CityItem";
+import Message from "./Message";
+import styles from "./CountryList.module.css";
+
+function CountryList({ cities, isLoading }) {
+  if (isLoading) return <Spinner />;
+
+  if (!cities.length)
+    return (
+      <Message message="Add your first city by clicking on a city on the map" />
+    );
+  return (
+    <ul className={styles.countriesList}>
+      {cities.map((city) => (
+        <CityItem city={city} key={city.id} />
+      ))}
+    </ul>
+  );
+}
+
+export default CountryList;
