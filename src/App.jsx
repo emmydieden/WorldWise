ae7493cff0c17c0363e579d738c2f5232e3284d4
emmydieden <emmy.dieden@gmail.com>
2024-03-20 15:38:39 +0100

    set up context for fake authentication

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,9 @@ import AppLayout from "./pages/AppLayout";
 import PageNotFound from "./pages/PageNotFound";
 import Login from "./pages/Login";
 import CityList from "./components/CityList";
+import CountryList from "./components/CountryList";
 import City from "./components/City";
 import Form from "./components/Form";
-
-import CountryList from "./components/CountryList";
 import { CitiesProvider } from "./contexts/CitiesContext";
 
 function App() {
